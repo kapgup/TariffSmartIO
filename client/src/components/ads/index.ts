3f0a69259227f17300cc99917bf9d4c0fb1b820b
KapilGupta15 <24954191-KapilGupta15@users.noreply.replit.com>
2025-04-07 17:03:12 +0000

    Restored to '27799fb1ab0b1b2ad48fd05537b339bc523bf84e'
    Replit-Restored-To: 27799fb1ab0b1b2ad48fd05537b339bc523bf84e

--- client/src/components/ads/index.ts
+++
@@ -1,9 +0,0 @@
-export { AdComponent } from './AdComponent';
-export { AdProvider, useAds } from './AdProvider';
-export { 
-  FooterAd, 
-  SidebarAd, 
-  InContentAd, 
-  PostCalculationAd,
-  ResponsiveAd
-} from './AdPlacements';
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,6 @@ import { TariffOverview } from "@/components/home/TariffOverview";
 import { SignupCTA } from "@/components/shared/SignupCTA";
 import { useEffect } from "react";
 import { pageView } from "@/lib/analytics";
-import { ResponsiveAd } from "@/components/ads";
 
 export default function Home() {
   useEffect(() => {
@@ -15,13 +14,8 @@ export default function Home() {
     <>
       <main className="flex-1">
         <Hero />
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <TariffOverview />
-          <div className="mt-6 mb-10" style={{ height: "125px" }}> {/* Reduced height by 50% */}
-            <ResponsiveAd slot="homepage-content-ad" />
-          </div>
-          <SignupCTA />
-        </div>
+        <TariffOverview />
+        <SignupCTA />
       </main>
       <Footer />
     </>
